cf9bdbbe83652724b624f629ded45f38769afb16
windsing <117965575+wochenlong@users.noreply.github.com>
2023-04-08 22:29:00 +0800

    Update index.ts
    增加了重置记忆功能，优化了 usrid

--- glm-girlfriend/src/index.ts
+++ glm-girlfriend/src/index.ts
@@ -1,26 +1,25 @@
-import { Context, Schema, Session } from "koishi";
+import { Context, Schema, Session, h } from "koishi";
 
 export const name = "glm-girlfriend";
 
 export const usage = `
 ### 用前需知
 
-本插件基于TomLBZ的[openai](https://github.com/TomLBZ/koishi-plugin-openai)插件进行开发
+本插件基于 [TomLBZ](https://github.com/TomLBZ) 的 [koishi-plugin-openai](https://github.com/TomLBZ/koishi-plugin-openai) 插件进行开发。
 
-机器有三种情况会回复你的对话：
+**机器有三种情况会回复你的对话：**
 
-1.被叫到名字（botname）
+1. 被叫到名字
+2. 自己被@
+3. 随机回复
 
-2.自己被@
-
-3.随机回复（randomReplyFrequency）
+输入"重置对话" 或"清除记忆"可以清除机器人的记忆
 
 ### 服务器地址格式
 
-应为https://服务器地址/chatglm?"
+应为 \`https://服务器地址/chatglm?\`
 
-自建后端部署chatglm后需要安装配套的api文件，[点击下载](https://github.com/wochenlong/ChatGLM-6B-with-flask-api)
-
+自建后端部署 chatglm 后需要安装配套的 api 文件，[点击下载](https://github.com/wochenlong/ChatGLM-6B-with-flask-api)。
 `;
 
 export interface Config {
@@ -42,7 +41,8 @@ export const Config: Schema<Config> = Schema.object({
     .max(10)
     .min(0)
     .description("机器人未被直接呼叫（未被@、未被直呼其名）时的随机回复概率")
-    .default(1),
+    .step(0.1)
+    .default(0.5),
 });
 
 const conversation = new Map<string, Map<string, string>>();
@@ -68,18 +68,35 @@ function getReplyCondition(session: Session, config: Config) {
 }
 
 export function apply(ctx: Context, config: Config) {
+  // 定义一个函数，用于获取指定字符串从指定位置到末尾的子串
+
   // 在 koishi 的中间件栈中添加一个中间件函数
   ctx.middleware(async (session, next) => {
+    const apiAddress = config.apiAddress;
+    let msgmemory = session.content;
     // 如果当前消息来自 koishi 机器人本身，则忽略该消息
-    if (ctx.bots[session.uid]) return;
+    if (ctx.bots[session.uid]) {
+      return;
+    }
     // 如果消息符合回复条件
     if (getReplyCondition(session, config)) {
-      // 向 API 发起请求以获取回复
-      const apiAddress = config.apiAddress;
-      const userText = session.content;
+      // 获取消息历史记录中的最后四个字符，并去掉两端的空格
+      // 如果消息历史记录为 "重置对话" 或 "重置"，则将消息内容设置为 "clear"
+      let userText = session.content;
+      let chat_id = 0;
+      if (msgmemory == "重置对话" || msgmemory == "清除记忆") {
+        userText = "clear";
+        chat_id = Math.floor(Math.random() * 1000000) + 1;
+        const resetMessage = `chatglm历史记忆已经重置，新的 chat_id 为 ${chat_id}`;
+        await session.send(resetMessage);
+      } else {
+        chat_id = Math.floor(Math.random() * 1000000) + 1;
+      }
+      const usrid = `&usrid=|channel_id=${session.channelId}|user_id=${session.userId}|chat_id=${chat_id}`;
       const session_id = `&msg=${encodeURIComponent(
         userText
-      )}&usrid=1000&source=glm-girlfriend`;
+      )}${usrid}&source=glm-girlfriend`;
+      // 向 API 发起请求以获取回复
       const response = await ctx.http.get(apiAddress + userText + session_id, {
         responseType: "text",
       });
