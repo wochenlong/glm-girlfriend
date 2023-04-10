185ea93b791a79740197a779892dbadc407a8e67
windsing <117965575+wochenlong@users.noreply.github.com>
2023-04-11 00:00:24 +0800

    Update index.ts
    使得对话id为固定值

--- glm-girlfriend/src/index.ts
+++ glm-girlfriend/src/index.ts
@@ -13,13 +13,21 @@ export const usage = `
 2. 自己被@
 3. 随机回复
 
-输入"重置对话" 或"清除记忆"可以清除机器人的记忆
+
+
+如果发现机器人说话吞吞吐吐的，可能是达到最大对话字数
+
+这时输入"重置对话" 或"清除记忆"可以清除机器人的记忆
 
 ### 服务器地址格式
 
 应为 \`https://服务器地址/chatglm?\`
 
 自建后端部署 chatglm 后需要安装配套的 api 文件，[点击下载](https://github.com/wochenlong/ChatGLM-6B-with-flask-api)。
+
+### 更新日志：
+0.6.2：修复了source参数没有发送的错误，不会再显示“无来源的请求，请更新插件”
+
 `;
 
 export interface Config {
@@ -27,6 +35,7 @@ export interface Config {
   botname: string;
   memoryShortLength: number;
   randomReplyFrequency: number;
+  chatid: number;
 }
 
 export const Config: Schema<Config> = Schema.object({
@@ -43,6 +52,10 @@ export const Config: Schema<Config> = Schema.object({
     .description("机器人未被直接呼叫（未被@、未被直呼其名）时的随机回复概率")
     .step(0.1)
     .default(0.5),
+  chatid: Schema.number()
+    .description("你的对话id，1到10万范围内随机填一位,必填")
+
+    .required(),
 });
 
 const conversation = new Map<string, Map<string, string>>();
@@ -68,8 +81,6 @@ function getReplyCondition(session: Session, config: Config) {
 }
 
 export function apply(ctx: Context, config: Config) {
-  // 定义一个函数，用于获取指定字符串从指定位置到末尾的子串
-
   // 在 koishi 的中间件栈中添加一个中间件函数
   ctx.middleware(async (session, next) => {
     const apiAddress = config.apiAddress;
@@ -83,23 +94,25 @@ export function apply(ctx: Context, config: Config) {
       // 获取消息历史记录中的最后四个字符，并去掉两端的空格
       // 如果消息历史记录为 "重置对话" 或 "重置"，则将消息内容设置为 "clear"
       let userText = session.content;
-      let chat_id = 0;
-      if (msgmemory == "重置对话" || msgmemory == "清除记忆") {
+      let chat_id = config.chatid; // chat_id 的默认值为 100000
+
+      if (msgmemory === "重置对话" || msgmemory === "清除记忆") {
         userText = "clear";
-        chat_id = Math.floor(Math.random() * 1000000) + 1;
+        // 如果是重置对话或清除记忆，chat_id 加上一个 100000 以内的随机数
+        chat_id += Math.floor(Math.random() * 100000);
         const resetMessage = `chatglm历史记忆已经重置，新的 chat_id 为 ${chat_id}`;
         await session.send(resetMessage);
-      } else {
-        chat_id = Math.floor(Math.random() * 1000000) + 1;
       }
-      const usrid = `&usrid=|channel_id=${session.channelId}|user_id=${session.userId}|chat_id=${chat_id}`;
+
+      const usrid = `&usrid=${chat_id}`;
       const session_id = `&msg=${encodeURIComponent(
         userText
       )}${usrid}&source=glm-girlfriend`;
-      // 向 API 发起请求以获取回复
+
       const response = await ctx.http.get(apiAddress + userText + session_id, {
         responseType: "text",
       });
+
       // 如果该用户没有历史对话记录，则在 Map 对象中为该用户创建一条记录
       if (!conversation.has(session.uid)) {
         conversation.set(session.uid, new Map<string, string>());
@@ -112,6 +125,7 @@ export function apply(ctx: Context, config: Config) {
       }
       // 将当前消息内容和回复内容保存到该用户的对话记录中
       conv.set(session.content, response);
+
       // 返回回复内容
       return response;
     }
